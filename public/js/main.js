02aef5b820600ac57430bb07dd7b53978bd338ae
Gary Acers <gacers@hugeinc.com>
2016-09-06 01:20:46 -0400

    Add project files

---
+++ public/js/main.js
@@ -0,0 +1,79 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function(event) { 
+	JS.init();
+});
+
+'use strict';
+
+var JS = JS || {};
+
+JS.init = function (config) {
+	/*
+	* Handles Slide Out transitions and
+	* showing/hiding
+	*/
+	this.slideOutComponent = this.slideOut({});
+	this.slideOutComponent.init(); //Initializes slide out logic
+}
+
+/*global $:false*/
+
+'use strict';
+
+var JS = JS || {};
+
+JS.slideOut = function () {
+	var $body = document.body,
+		$webItem = document.getElementsByClassName('web-site'),
+		$slideOut = document.getElementsByClassName('slide-out')[0],
+		$container = document.getElementsByClassName('slide-out-container')[0],
+		$closeButton = document.getElementsByClassName('close-slide-out')[0],
+		OPEN = '_slide-out-open',
+		isOpen = false;
+
+	var closeSlideOut = function() {
+		if (isOpen) {
+			$body.classList.remove(OPEN);
+
+			isOpen = false;
+		}
+	};
+
+	var closeListen = function() {
+		$slideOut.addEventListener('click', function(e){
+			if (!$container.contains(event.target)) {
+				closeSlideOut();
+			} else if (e.target == $closeButton){
+				closeSlideOut();
+			}
+		});		
+	};
+
+	var activateSlideOut = function() {
+		$body.classList.add(OPEN);
+
+		isOpen = true;
+
+		closeListen();
+	};
+
+	var websiteActivate = function() {		
+		for (var i=0; i < $webItem.length; i++) {
+
+			$webItem[i].addEventListener('click', function() {
+				this.focus();
+
+				activateSlideOut();
+			});
+		}
+	};
+
+	var init = function() {
+		websiteActivate();
+    };
+
+    return {
+        init: init
+    };
+};
