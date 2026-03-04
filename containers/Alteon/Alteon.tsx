06853cf665bda1a9c804d9a02b5ee8ebd21ab3ad
Gary Acers <gary.acers@gmail.com>
2026-03-04 17:48:57 -0500

    fix alteon link

--- containers/Alteon/Alteon.tsx
+++ containers/Alteon/Alteon.tsx
@@ -45,7 +45,7 @@ const Alteon = () => {
           description="Alteon was built on top of NextJS and Framer Motion. The site utilized Redux to track global state and to trigger animations and page transitions. Documentation was handled in Storybook."
           years='2020 - 2021'
           skills="NextJS React, Redux, Framer Motion, Storybook, PostCSS, CSS Modules, Javascript, Figma, Context, Contentful"
-          url="https://www.alteon.io/"
+          url="https://alteon.io/"
         />
       }
     />
